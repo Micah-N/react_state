8c3d0eb783790ab85f4b37de47479b7da862a6f6
Micah-N <mnetz@2ndwatch.com>
2017-07-03 11:55:04 -0700

    Added files

---
+++ src/Toggle.jsx
@@ -0,0 +1,14 @@
+const green = '#39D1B4';
+const yellow = '#FFD712';
+
+class Toggle extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Change my color
+        </h1>
+      </div>
+    );
+  }
+}
